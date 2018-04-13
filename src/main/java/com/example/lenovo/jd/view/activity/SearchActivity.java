b00e1fead52dd3dbd8286199dabb211933e6b5c9
农夫三拳 <1227522057@qq.com>
2018-04-13 21:00:51 +0800

    仿京东

---
+++ src/main/java/com/example/lenovo/jd/view/activity/SearchActivity.java
@@ -0,0 +1,31 @@
+package com.example.lenovo.jd.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lenovo.jd.R;
+import com.example.lenovo.jd.view.base.BaseActivity;
+import com.example.lenovo.jd.view.base.BasePresenter;
+
+public class SearchActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_search;
+    }
+
+    @Override
+    protected BasePresenter getPresenter() {
+        return null;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void getData() {
+
+    }
+}
