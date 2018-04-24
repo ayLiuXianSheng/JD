5c190e6440e14422faa7eae1dad55ba9c13723d1
农夫三拳 <1227522057@qq.com>
2018-04-24 08:48:38 +0800

    仿京东

--- src/main/java/com/example/lenovo/jd/view/activity/SearchActivity.java
+++ src/main/java/com/example/lenovo/jd/view/activity/SearchActivity.java
@@ -2,12 +2,16 @@ package com.example.lenovo.jd.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.lenovo.jd.R;
 import com.example.lenovo.jd.view.base.BaseActivity;
 import com.example.lenovo.jd.view.base.BasePresenter;
 
 public class SearchActivity extends BaseActivity {
+
+    private ImageView image_return;
 
     @Override
     protected int getLayoutId() {
@@ -21,7 +25,13 @@ public class SearchActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-
+        image_return = findViewById(R.id.image_return);
+        image_return.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     @Override
